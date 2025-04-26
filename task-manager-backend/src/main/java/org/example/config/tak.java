6ca910d115d765d0f82a991ed17401e8beb97868
Marbur12 <M.burczynski12@gmail.com>
2025-04-26 21:13:08 +0200

    deleting uneccesary classes

--- task-manager-backend/src/main/java/org/example/config/tak.java
+++
@@ -1,9 +0,0 @@
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-public class tak {
-    public static void main(String[] args) {
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        String encodedPassword = encoder.encode("admin123");
-        System.out.println(encodedPassword); // Wydrukuj zaszyfrowane hasło
-    }
-}

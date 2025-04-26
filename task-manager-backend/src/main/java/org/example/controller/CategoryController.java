5c4d6b15eafa05f216115ba00a4aae1d67c18b49
Nikodem <nikodem.sd@gmail.com>
2025-04-26 19:57:45 +0200

    fixed controllers

--- task-manager-backend/src/main/java/org/example/controller/CategoryController.java
+++ task-manager-backend/src/main/java/org/example/controller/CategoryController.java
@@ -29,7 +29,7 @@ public class CategoryController {
 
     @GetMapping
     public List<CategoryDTO> listCategories(@AuthenticationPrincipal UserDetails userDetails) {
-        User user = getUserFromDetails(userDetails);
+        User user = userService.getUserFromDetails(userDetails);
         return categoryService.getCategoriesForUser(user)
                 .stream()
                 .map(category -> new CategoryDTO(category.getId(), category.getName()))
@@ -39,7 +39,7 @@ public class CategoryController {
     @PostMapping
     public CategoryDTO createCategory(@AuthenticationPrincipal UserDetails userDetails,
                                       @RequestParam("name") String name) {
-        User user = getUserFromDetails(userDetails);
+        User user = userService.getUserFromDetails(userDetails);
         var category = categoryService.createCategory(user, name);
         return new CategoryDTO(category.getId(), category.getName());
     }
@@ -47,26 +47,15 @@ public class CategoryController {
     @GetMapping("/{id}")
     public CategoryDTO getCategory(@AuthenticationPrincipal UserDetails userDetails,
                                @PathVariable("id") Long id) {
-        User user = getUserFromDetails(userDetails);
+        User user = userService.getUserFromDetails(userDetails);
         return categoryService.getCategoryByIdAndUser(user, id);
     }
 
     @DeleteMapping("/{id}")
     public void deleteCategory(@AuthenticationPrincipal UserDetails userDetails,
                                @PathVariable("id") Long id) {
-        User user = getUserFromDetails(userDetails);
+        User user = userService.getUserFromDetails(userDetails);
         categoryService.deleteCategory(id, user);
-    }
-
-    private User getUserFromDetails(UserDetails userDetails) {
-
-        User user = userService.findByUsername(userDetails.getUsername());
-
-        if (user == null) {
-            throw new RuntimeException("User not found");
-        }
-
-        return user;
     }
 
 }

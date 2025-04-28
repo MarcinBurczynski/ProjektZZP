31ccd02f3dd5b7bdf8a023acdaf19274c6669e38
Damian Polkowski <damianpolkowski13@gmail.com>
2025-04-28 22:20:16 +0200

    Implemented helper functions for posting to API

---
+++ task-manager-frontend/frontend/src/utils/posting_helper.ts
@@ -0,0 +1,58 @@
+import apiClient from "../environments/axios";
+
+export async function postCategory(name: string): Promise<boolean> {
+  try {
+    const response = await apiClient.post('/api/categories', {
+        name: name
+    });
+    if (response.status === 200) {
+      return true;
+    } else {
+      console.error('Failed to post category:', response.status);
+      return false;
+    }
+  } catch (error) {
+    console.error('Error posting category:', error);
+    return false;
+  }
+}
+
+export async function postTask(title: string, description: string, status: string, categoryId: number, userId: number): Promise<boolean> {
+  try {
+    const response = await apiClient.post('/api/tasks', {
+        title: title,
+        description: description,
+        status: status,
+        categoryId: categoryId,
+        userId: userId
+    });
+    if (response.status === 200) {
+      return true;
+    } else {
+      console.error('Failed to post task:', response.status);
+      return false;
+    }
+  } catch (error) {
+    console.error('Error posting task:', error);
+    return false;
+  }
+}
+
+export async function postUser(username: string, password: string, role: number): Promise<boolean> {
+  try {
+    const response = await apiClient.post('/api/users', {
+        username: username,
+        password: password,
+        role: role
+    });
+    if (response.status === 200) {
+      return true;
+    } else {
+      console.error('Failed to fetch users:', response.status);
+      return false;
+    }
+  } catch (error) {
+    console.error('Error fetching users:', error);
+    return false;
+  }
+}
